eddb3c8f23def03318413450d57e217f3654363b
laziszn <115390022+laziszn@users.noreply.github.com>
2026-08-16 12:13:45 +0530

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+/*
 const popup = document.getElementById("supportPopup");
 
 document.getElementById("openSupportPopup").addEventListener("click", () => {
@@ -9,6 +10,28 @@ document.getElementById("closeSupportPopup").addEventListener("click", () => {
 });
 
 window.addEventListener("click", (e) => {
+  if (e.target === popup) {
+    popup.style.display = "none";
+  }
+});*/
+
+const popup = document.getElementById("supportPopup");
+
+document.querySelectorAll(".open-support-popup").forEach((button) => {
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    popup.style.display = "flex";
+  });
+});
+
+document
+  .getElementById("closeSupportPopup")
+  .addEventListener("click", function () {
+    popup.style.display = "none";
+  });
+
+window.addEventListener("click", function (e) {
   if (e.target === popup) {
     popup.style.display = "none";
   }
